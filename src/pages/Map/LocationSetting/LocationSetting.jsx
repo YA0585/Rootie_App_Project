2e21f120d734225d64a4e359fc2435c30630bb78
YA0585 <seya0510@gmail.com>
2026-05-29 13:06:52 +0900

    fix: use absolute positioning for loc-map container to bypass flexbox height collapse issue

--- src/pages/Map/LocationSetting/LocationSetting.jsx
+++ src/pages/Map/LocationSetting/LocationSetting.jsx
@@ -63,7 +63,7 @@ const LocationSetting = ({ onBack, navBar }) => {
 
             {/* Map area */}
             <div className="loc-map-area">
-                <div id="loc-map" style={{ width: "100%", height: "100%" }}></div>
+                <div id="loc-map" style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}></div>
                 {/* Map pin label */}
                 <div className="loc-pin-wrapper">
                     <div className="loc-pin-label">강남역사거리</div>
