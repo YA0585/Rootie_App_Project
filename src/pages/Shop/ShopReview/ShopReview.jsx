50aa4fd9f52d39907ac969b6657e0e5066da3b6d
YA0585 <seya0510@gmail.com>
2026-05-29 18:26:57 +0900

    Fix shopName text wrap issue in ShopTop component

--- src/pages/Shop/ShopReview/ShopReview.jsx
+++ src/pages/Shop/ShopReview/ShopReview.jsx
@@ -4,16 +4,18 @@ import ShopTabBar from "../../../components/ShopTabBar/ShopTabBar";
 import ShopTop from "../../../components/ShopTop/ShopTop";
 
 const StarIcon = ({ filled = true, size = 20 }) => (
-    <svg width={size} height={size} viewBox="0 0 24 24" fill={filled ? "#6AB43A" : "none"} stroke="#6AB43A" strokeWidth="1.5">
-        <polygon points="12,2 15.09,8.26 22,9.27 17,14.14 18.18,21.02 12,17.77 5.82,21.02 7,14.14 2,9.27 8.91,8.26" />
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13.7276 3.44418L15.4874 6.99288C15.7274 7.48687 16.3673 7.9607 16.9073 8.05143L20.0969 8.58575C22.1367 8.92853 22.6167 10.4206 21.1468 11.8925L18.6671 14.3927C18.2471 14.8161 18.0172 15.6327 18.1471 16.2175L18.8571 19.3125C19.417 21.7623 18.1271 22.71 15.9774 21.4296L12.9877 19.6452C12.4478 19.3226 11.5579 19.3226 11.0079 19.6452L8.01827 21.4296C5.8785 22.71 4.57865 21.7522 5.13859 19.3125L5.84851 16.2175C5.97849 15.6327 5.74852 14.8161 5.32856 14.3927L2.84884 11.8925C1.389 10.4206 1.85895 8.92853 3.89872 8.58575L7.08837 8.05143C7.61831 7.9607 8.25824 7.48687 8.49821 6.99288L10.258 3.44418C11.2179 1.51861 12.7777 1.51861 13.7276 3.44418Z" fill="#6AB43A" />
     </svg>
+
 );
 
 const ThumbsUpIcon = () => (
-    <svg width={16} height={16} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3H14z" />
-        <path d="M7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3" />
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2 10.4115C2 9.50179 2.7374 8.7644 3.64703 8.7644C5.01146 8.7644 6.11757 9.87049 6.11757 11.235V14.5291C6.11757 15.8936 5.01146 16.9997 3.64703 16.9997C2.7374 16.9997 2 16.2623 2 15.3526V10.4115Z" stroke="#2F2F2F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M13.1 6.54633L12.8807 7.25463C12.701 7.83503 12.6112 8.12523 12.6803 8.35442C12.7362 8.5398 12.8589 8.69965 13.0261 8.80481C13.2328 8.93477 13.5455 8.93477 14.1709 8.93477H14.5036C16.6201 8.93477 17.6783 8.93477 18.1782 9.56131C18.2353 9.63287 18.2861 9.70905 18.33 9.78893C18.7145 10.4876 18.2774 11.4289 17.4031 13.3115C16.6008 15.0391 16.1995 15.9029 15.4547 16.4113C15.3826 16.4605 15.3085 16.507 15.2326 16.5504C14.4483 17 13.4768 17 11.5334 17H11.112C8.75761 17 7.58047 17 6.84907 16.2914C6.11768 15.5826 6.11768 14.442 6.11768 12.1609V11.3591C6.11768 10.1603 6.11768 9.5609 6.33042 9.01226C6.54317 8.46362 6.95054 8.01253 7.76527 7.11034L11.1346 3.37929C11.2191 3.28572 11.2613 3.23892 11.2986 3.2065C11.6463 2.90388 12.183 2.93794 12.4871 3.28194C12.5197 3.31879 12.5552 3.37051 12.6264 3.47397C12.7377 3.63581 12.7934 3.71673 12.842 3.79689C13.2763 4.51458 13.4077 5.36712 13.2088 6.17648C13.1865 6.26688 13.1577 6.36008 13.1 6.54633Z" stroke="#2F2F2F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
     </svg>
+
 );
 
 const MoreVertIcon = () => (
@@ -43,18 +45,50 @@ const BookmarkIcon = () => (
 );
 
 const EditIcon = () => (
-    <svg width={16} height={16} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4.10807 13.3516L3.5 17L7.14845 16.3919C7.78201 16.2864 8.36675 15.9855 8.82093 15.5312L17.0487 7.30335C17.6504 6.70161 17.6504 5.72603 17.0487 5.12431L15.3757 3.4513C14.7739 2.84956 13.7982 2.84957 13.1965 3.45132L4.96876 11.6792C4.51459 12.1334 4.21367 12.7181 4.10807 13.3516Z" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M12.0552 5.33331L15.1663 8.44442" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
     </svg>
+
 );
 
 const PlantCoin = () => (
-    <svg width={14} height={14} viewBox="0 0 14 14" fill="none">
-        <circle cx="7" cy="7" r="7" fill="#6AB43A" />
-        <path d="M4.5 9C5.5 7 7 6 9.5 5.5C9 8 7.5 9.5 5 10" stroke="#fff" strokeWidth="1" fill="none" />
-        <path d="M7 9.5V11" stroke="#fff" strokeWidth="1" strokeLinecap="round" />
+    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_541_14446)">
+            <path d="M14 7C14 10.866 10.866 14 7 14C3.13401 14 0 10.866 0 7C0 3.13401 3.13401 0 7 0C10.866 0 14 3.13401 14 7Z" fill="#6AB43A" />
+        </g>
+        <path d="M11.4548 7.00003C11.4548 9.81166 9.17549 12.0909 6.36386 12.0909C3.55223 12.0909 1.27295 9.81166 1.27295 7.00003C1.27295 4.1884 3.55223 1.90912 6.36386 1.90912C9.17549 1.90912 11.4548 4.1884 11.4548 7.00003Z" fill="#54902D" />
+        <g filter="url(#filter1_ii_541_14446)">
+            <path d="M12.091 7.00003C12.091 9.81166 9.81172 12.0909 7.00009 12.0909C4.18846 12.0909 1.90918 9.81166 1.90918 7.00003C1.90918 4.1884 4.18846 1.90912 7.00009 1.90912C9.81172 1.90912 12.091 4.1884 12.091 7.00003Z" fill="#8BE750" />
+        </g>
+        <path d="M4.45459 5.72729L5.72732 8.59093L6.84095 5.72729L7.95459 8.59093L9.22732 5.72729" stroke="#6AB43A" stroke-width="0.954545" stroke-linecap="round" stroke-linejoin="round" />
+        <defs>
+            <filter id="filter0_d_541_14446" x="0" y="0" width="14.6364" height="14.3182" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                <feOffset dx="0.636364" dy="0.318182" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0.215529 0 0 0 0 0.376602 0 0 0 0 0.111049 0 0 0 1 0" />
+                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_541_14446" />
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_541_14446" result="shape" />
+            </filter>
+            <filter id="filter1_ii_541_14446" x="1.90918" y="1.90912" width="10.1816" height="10.1818" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                <feOffset dx="0.318182" dy="0.318182" />
+                <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0.393443 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0" />
+                <feBlend mode="normal" in2="shape" result="effect1_innerShadow_541_14446" />
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                <feOffset dx="-0.318182" dy="-0.318182" />
+                <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0.671448 0 0 0 0 1 0 0 0 0 0.458333 0 0 0 1 0" />
+                <feBlend mode="normal" in2="effect1_innerShadow_541_14446" result="effect2_innerShadow_541_14446" />
+            </filter>
+        </defs>
     </svg>
+
 );
 
 const reviews = [
@@ -107,14 +141,14 @@ export default function ShopReview({ onBack, onGoToHome, onGoToPrice, onGoToRevi
             <ShopTop onBack={onBack} />
 
             {/* Tabs */}
-            <ShopTabBar 
-                activeTab={activeTab} 
+            <ShopTabBar
+                activeTab={activeTab}
                 onTabChange={(tab) => {
                     setActiveTab(tab);
                     if (tab === "홈" && onGoToHome) onGoToHome();
                     if (tab === "가격" && onGoToPrice) onGoToPrice();
                     if (tab === "후기" && onGoToReview) onGoToReview();
-                }} 
+                }}
             />
 
             {/* Write Review CTA */}
@@ -162,11 +196,11 @@ export default function ShopReview({ onBack, onGoToHome, onGoToPrice, onGoToRevi
                             className={`filter-photo ${photoOnly ? "filter-photo-active" : ""}`}
                             onClick={() => setPhotoOnly(!photoOnly)}
                         >
-                            <svg width={14} height={14} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-                                <rect x="3" y="3" width="18" height="18" rx="2" />
-                                <circle cx="8.5" cy="8.5" r="1.5" />
-                                <polyline points="21 15 16 10 5 21" />
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11.4783 3.11304H9.44348C6.406 3.11304 4.88725 3.11304 3.94362 4.05666C3 5.00029 3 6.51903 3 9.55652C3 12.594 3 14.1127 3.94362 15.0564C4.88725 16 6.406 16 9.44348 16C12.4809 16 13.9997 16 14.9434 15.0564C15.887 14.1127 15.887 12.594 15.887 9.55652V8.19999" stroke="#8F8F8F" stroke-width="1.5" stroke-linecap="round" />
+                                <path d="M7.06885 7.4087L9.44276 9.78261L15.5472 3" stroke="#8F8F8F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
+
                             <span>사진 후기만</span>
                         </button>
                     </div>
